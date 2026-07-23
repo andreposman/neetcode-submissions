cefc1ae8f83a5c63fca82edbdc288093a390c17a
André Posman <andre.posman@gmail.com>
2026-07-23 00:02:55 -0300

    Update: two-integer-sum - submission-1

--- Data Structures & Algorithms/two-integer-sum/submission-1.go
+++ Data Structures & Algorithms/two-integer-sum/submission-1.go
@@ -1,4 +1,14 @@
+/** 
+
+#1 solution, brute force: 6min41s. 
+Time: O(nˆ2), two fors operating through array
+Space: O(1), helper slice, is the same size if n grows (0,1)
+
+
+#2 solution
+**/
 func twoSum(nums []int, target int) []int {
+
 	var twoSum []int
     
 	for i := 0; i <= len(nums)-1; i++ {
@@ -7,8 +17,6 @@ func twoSum(nums []int, target int) []int {
 				twoSum = append(twoSum, i, j)
 			}
 		}
-
-	
 	}
 	return twoSum
 }
