cb1ead26b2e7e50b19d2410e3a245d33bd4e2ac7
André Posman <andre.posman@gmail.com>
2026-07-22 23:46:49 -0300

    Add: two-integer-sum - submission-1

---
+++ Data Structures & Algorithms/two-integer-sum/submission-1.go
@@ -0,0 +1,14 @@
+func twoSum(nums []int, target int) []int {
+	var twoSum []int
+    
+	for i := 0; i <= len(nums)-1; i++ {
+		for j:= i+1; j <= len(nums)-1; j++{
+			if nums[i] + nums[j] == target {
+				twoSum = append(twoSum, i, j)
+			}
+		}
+
+	
+	}
+	return twoSum
+}
