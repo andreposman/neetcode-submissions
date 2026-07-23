a90a0cf16ece3635de41034a1651fec8b8040cf3
André Posman <andre.posman@gmail.com>
2026-07-22 23:48:09 -0300

    Update: two-integer-sum - submission-0

--- Data Structures & Algorithms/two-integer-sum/submission-0.go
+++ Data Structures & Algorithms/two-integer-sum/submission-0.go
@@ -1,15 +1,15 @@
+//#1 solution, brute force: 6min41s. O(nˆ2)
+
 func twoSum(nums []int, target int) []int {
+
 	var twoSum []int
     
 	for i := 0; i <= len(nums)-1; i++ {
 		for j:= i+1; j <= len(nums)-1; j++{
 			if nums[i] + nums[j] == target {
-				fmt.Printf("nums[i]:%d + nums[j]:%d == %d", nums[i], nums[j], target)
 				twoSum = append(twoSum, i, j)
 			}
 		}
-
-	
 	}
 	return twoSum
 }
